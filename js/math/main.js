eb1e0d18843fcdf229e21681614893c7b00cb53b
Juan David Omen <anvidmen@gmail.com>
2022-04-20 21:09:53 +0200

    removed unnecessary file

--- js/math/main.js
+++
@@ -1,9 +0,0 @@
-
-const randomNumber = () => {
-    let number = Math.round(Math.random());
-    if (number === 0) return "🌝";
-    return "🌞";
-}
-
-let result = randomNumber();
-console.log(result);
