c3f11e5500a8e496818503df4f838f8a45ed3565
Juan David Omen <anvidmen@gmail.com>
2022-04-21 09:56:39 +0200

    added for-while exercise

---
+++ js/for-while/main.js
@@ -0,0 +1,16 @@
+for (let index = 0; index < 10; index++) {
+    const element = index + 1;
+    console.log(`Nº: ${element}, I love Code`)
+} 
+
+console.log("--------------------")
+
+let text = ""
+let i = 0
+
+while (i < 10) {
+    text += " I love Code ";
+    i++;
+}
+
+console.log(text)
